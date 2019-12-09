def612030e8a1759086681f47b8db671524841fc
Dominik Nakamura <dnaka91@gmail.com>
2019-12-09 11:01:37 +0900

    Simplify function calls in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,40 @@
+use std::fmt::Display;
+
 use anyhow::Result;
 
 fn main() -> Result<()> {
     println!("=== Advent of Code ===");
+
+    run_y2019()?;
+
+    Ok(())
+}
+
+fn run_y2019() -> Result<()> {
+    use aoc::y2019::*;
+
     println!("Year 2019");
 
-    println!("  Day 01");
-    println!("    Part 1: {}", aoc::y2019::d01::solve_part_one(aoc::y2019::d01::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d01::solve_part_two(aoc::y2019::d01::INPUT)?);
+    run(1, d01::solve_part_one, d01::solve_part_two, d01::INPUT)?;
+    run(2, d02::solve_part_one, d02::solve_part_two, d02::INPUT)?;
+    run(3, d03::solve_part_one, d03::solve_part_two, d03::INPUT)?;
+    run(4, d04::solve_part_one, d04::solve_part_two, d04::INPUT)?;
+    run(5, d05::solve_part_one, d05::solve_part_two, d05::INPUT)?;
+    run(6, d06::solve_part_one, d06::solve_part_two, d06::INPUT)?;
+    run(8, d08::solve_part_one, d08::solve_part_two, d08::INPUT)?;
 
-    println!("  Day 02");
-    println!("    Part 1: {}", aoc::y2019::d02::solve_part_one(aoc::y2019::d02::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d02::solve_part_two(aoc::y2019::d02::INPUT)?);
+    Ok(())
+}
 
-    println!("  Day 03");
-    println!("    Part 1: {}", aoc::y2019::d03::solve_part_one(aoc::y2019::d03::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d03::solve_part_two(aoc::y2019::d03::INPUT)?);
-
-    println!("  Day 04");
-    println!("    Part 1: {}", aoc::y2019::d04::solve_part_one(aoc::y2019::d04::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d04::solve_part_two(aoc::y2019::d04::INPUT)?);
-
-    println!("  Day 05");
-    println!("    Part 1: {}", aoc::y2019::d05::solve_part_one(aoc::y2019::d05::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d05::solve_part_two(aoc::y2019::d05::INPUT)?);
-
-    println!("  Day 06");
-    println!("    Part 1: {}", aoc::y2019::d06::solve_part_one(aoc::y2019::d06::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d06::solve_part_two(aoc::y2019::d06::INPUT)?);
-
-    println!("  Day 08");
-    println!("    Part 1: {}", aoc::y2019::d08::solve_part_one(aoc::y2019::d08::INPUT)?);
-    println!("    Part 2: {}", aoc::y2019::d08::solve_part_two(aoc::y2019::d08::INPUT)?);
-
+fn run<P1, T1, P2, T2>(day: u8, part1: P1, part2: P2, input: &str) -> Result<()>
+where
+    P1: Fn(&str) -> Result<T1>,
+    P2: Fn(&str) -> Result<T2>,
+    T1: Display,
+    T2: Display,
+{
+    println!("  Day {:02}", day);
+    println!("    Part 1: {}", part1(input)?);
+    println!("    Part 2: {}", part2(input)?);
     Ok(())
 }
