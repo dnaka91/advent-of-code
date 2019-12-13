d790d00b2447f42e945c5ddba6de7c21f6742c67
Dominik Nakamura <dnaka91@gmail.com>
2019-12-13 14:36:05 +0900

    Simplify benchmarks

--- benches/year2019.rs
+++ benches/year2019.rs
@@ -1,88 +1,27 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn y2019_d01(c: &mut Criterion) {
-    c.bench_function("Y2019 D01 P1", |b| {
-        b.iter(|| aoc::y2019::d01::solve_part_one(black_box(aoc::y2019::d01::INPUT)))
-    });
-    c.bench_function("Y2019 D01 P2", |b| {
-        b.iter(|| aoc::y2019::d01::solve_part_two(black_box(aoc::y2019::d01::INPUT)))
-    });
+fn y2019(c: &mut Criterion) {
+    use aoc::y2019::*;
+
+    run(c, 1, d01::solve_part_one, d01::solve_part_two, d01::INPUT);
+    run(c, 2, d02::solve_part_one, d02::solve_part_two, d02::INPUT);
+    run(c, 3, d03::solve_part_one, d03::solve_part_two, d03::INPUT);
+    run(c, 4, d04::solve_part_one, d04::solve_part_two, d04::INPUT);
+    run(c, 5, d05::solve_part_one, d05::solve_part_two, d05::INPUT);
+    run(c, 6, d06::solve_part_one, d06::solve_part_two, d06::INPUT);
+    run(c, 7, d07::solve_part_one, d07::solve_part_two, d07::INPUT);
+    run(c, 8, d08::solve_part_one, d08::solve_part_two, d08::INPUT);
+    run(c, 9, d09::solve_part_one, d09::solve_part_two, d09::INPUT);
 }
 
-fn y2019_d02(c: &mut Criterion) {
-    c.bench_function("Y2019 D02 P1", |b| {
-        b.iter(|| aoc::y2019::d02::solve_part_one(black_box(aoc::y2019::d02::INPUT)))
-    });
-    c.bench_function("Y2019 D02 P2", |b| {
-        b.iter(|| aoc::y2019::d02::solve_part_two(black_box(aoc::y2019::d02::INPUT)))
-    });
+fn run<P1, T1, P2, T2>(c: &mut Criterion, day: u8, part1: P1, part2: P2, input: &str)
+where
+    P1: Fn(&str) -> T1,
+    P2: Fn(&str) -> T2,
+{
+    c.bench_function(&format!("Y2019 D{:02} P1", day), |b| b.iter(|| part1(black_box(input))));
+    c.bench_function(&format!("Y2019 D{:02} P2", day), |b| b.iter(|| part2(black_box(input))));
 }
 
-fn y2019_d03(c: &mut Criterion) {
-    c.bench_function("Y2019 D03 P1", |b| {
-        b.iter(|| aoc::y2019::d03::solve_part_one(black_box(aoc::y2019::d03::INPUT)))
-    });
-    c.bench_function("Y2019 D03 P2", |b| {
-        b.iter(|| aoc::y2019::d03::solve_part_two(black_box(aoc::y2019::d03::INPUT)))
-    });
-}
-
-fn y2019_d04(c: &mut Criterion) {
-    c.bench_function("Y2019 D04 P1", |b| {
-        b.iter(|| aoc::y2019::d04::solve_part_one(black_box(aoc::y2019::d04::INPUT)))
-    });
-    c.bench_function("Y2019 D04 P2", |b| {
-        b.iter(|| aoc::y2019::d04::solve_part_two(black_box(aoc::y2019::d04::INPUT)))
-    });
-}
-
-fn y2019_d05(c: &mut Criterion) {
-    c.bench_function("Y2019 D05 P1", |b| {
-        b.iter(|| aoc::y2019::d05::solve_part_one(black_box(aoc::y2019::d05::INPUT)))
-    });
-    c.bench_function("Y2019 D05 P2", |b| {
-        b.iter(|| aoc::y2019::d05::solve_part_two(black_box(aoc::y2019::d05::INPUT)))
-    });
-}
-
-fn y2019_d06(c: &mut Criterion) {
-    c.bench_function("Y2019 D06 P1", |b| {
-        b.iter(|| aoc::y2019::d06::solve_part_one(black_box(aoc::y2019::d06::INPUT)))
-    });
-    c.bench_function("Y2019 D06 P2", |b| {
-        b.iter(|| aoc::y2019::d06::solve_part_two(black_box(aoc::y2019::d06::INPUT)))
-    });
-}
-
-fn y2019_d07(c: &mut Criterion) {
-    c.bench_function("Y2019 D07 P1", |b| {
-        b.iter(|| aoc::y2019::d07::solve_part_one(black_box(aoc::y2019::d07::INPUT)))
-    });
-    c.bench_function("Y2019 D07 P2", |b| {
-        b.iter(|| aoc::y2019::d07::solve_part_two(black_box(aoc::y2019::d07::INPUT)))
-    });
-}
-
-fn y2019_d08(c: &mut Criterion) {
-    c.bench_function("Y2019 D08 P1", |b| {
-        b.iter(|| aoc::y2019::d08::solve_part_one(black_box(aoc::y2019::d08::INPUT)))
-    });
-    c.bench_function("Y2019 D08 P2", |b| {
-        b.iter(|| aoc::y2019::d08::solve_part_two(black_box(aoc::y2019::d08::INPUT)))
-    });
-}
-
-fn y2019_d09(c: &mut Criterion) {
-    c.bench_function("Y2019 D09 P1", |b| {
-        b.iter(|| aoc::y2019::d09::solve_part_one(black_box(aoc::y2019::d09::INPUT)))
-    });
-    c.bench_function("Y2019 D09 P2", |b| {
-        b.iter(|| aoc::y2019::d09::solve_part_two(black_box(aoc::y2019::d09::INPUT)))
-    });
-}
-
-criterion_group!(
-    benches, y2019_d01, y2019_d02, y2019_d03, y2019_d04, y2019_d05, y2019_d06, y2019_d07,
-    y2019_d08, y2019_d09
-);
+criterion_group!(benches, y2019);
 criterion_main!(benches);
