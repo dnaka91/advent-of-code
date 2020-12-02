831c1d5dd1ad3d693ebff4292e7d99070cb80a4d
Dominik Nakamura <dnaka91@gmail.com>
2020-12-02 14:34:17 +0900

    Correct docs formatting of day 2

--- src/y2020/d02.rs
+++ src/y2020/d02.rs
@@ -24,7 +24,7 @@
 //! lowest and highest number of times a given letter must appear for the password to be valid. For
 //! example, `1-3 a` means that the password must contain a at least `1` time and at most `3` times.
 //!
-//! In the above example, **`2`** passwords are valid. The middle password, **cdefg**, is not; it
+//! In the above example, **`2`** passwords are valid. The middle password, `cdefg`, is not; it
 //! contains no instances of `b`, but needs at least `1`. The first and third passwords are valid:
 //! they contain one `a` or nine `c`, both within the limits of their respective policies.
 //!
