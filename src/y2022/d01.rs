fda054df6f02495265b70ceb7d9b1ef924529472
Dominik Nakamura <dnaka91@gmail.com>
2022-12-01 14:41:00 +0900

    Solve day 1 of 2022

--- src/y2022/d01.rs
+++ src/y2022/d01.rs
@@ -1,24 +1,140 @@
-//! #
+//! # Day 1: Calorie Counting
+//!
+//! Santa's reindeer typically eat regular reindeer food, but they need a lot of [magical energy] to
+//! deliver presents on Christmas. For that, their favorite snack is a special type of **star**
+//! fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition
+//! to the grove where the fruit grows.
+//!
+//! To supply enough magical energy, the expedition needs to retrieve a minimum of **fifty stars**
+//! by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide
+//! to grab any fruit you see along the way, just in case.
+//!
+//! Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent
+//! calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants **one
+//! star**. Good luck!
+//!
+//! The jungle must be too overgrown and difficult to navigate in vehicles or access from the air;
+//! the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin
+//! taking inventory of their supplies. One important consideration is food - in particular, the
+//! number of **Calories** each Elf is carrying (your puzzle input).
+//!
+//! The Elves take turns writing down the number of Calories contained by the various meals, snacks,
+//! rations, etc. that they've brought with them, one item per line. Each Elf separates their own
+//! inventory from the previous Elf's inventory (if any) by a blank line.
+//!
+//! For example, suppose the Elves finish writing their items' Calories and end up with the
+//! following list:
+//!
+//! ```txt
+//! 1000
+//! 2000
+//! 3000
+//!
+//! 4000
+//!
+//! 5000
+//! 6000
+//!
+//! 7000
+//! 8000
+//! 9000
+//!
+//! 10000
+//! ```
+//!
+//! This list represents the Calories of the food carried by five Elves:
+//!
+//! - The first Elf is carrying food with `1000`, `2000`, and `3000` Calories, a total of **`6000`**
+//!   Calories.
+//! - The second Elf is carrying one food item with **`4000`** Calories.
+//! - The third Elf is carrying food with `5000` and `6000` Calories, a total of **`11000`**
+//!   Calories.
+//! - The fourth Elf is carrying food with `7000`, `8000`, and `9000` Calories, a total of
+//!   **`24000`** Calories.
+//! - The fifth Elf is carrying one food item with **`10000`** Calories.
+//!
+//! In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd
+//! like to know how many Calories are being carried by the Elf carrying the **most** Calories. In
+//! the example above, this is **`24000`** (carried by the fourth Elf).
+//!
+//! Find the Elf carrying the most Calories. **How many total Calories is that Elf carrying?**
+//!
+//! [magical energy]: crate::y2018::d25
+//!
+//! ## Part Two
+//!
+//! By the time you calculate the answer to the Elves' question, they've already realized that the
+//! Elf carrying the most Calories of food might eventually **run out of snacks**.
+//!
+//! To avoid this unacceptable situation, the Elves would instead like to know the total Calories
+//! carried by the **top three** Elves carrying the most Calories. That way, even if one of those
+//! Elves runs out of snacks, they still have two backups.
+//!
+//! In the example above, the top three Elves are the fourth Elf (with `24000` Calories), then the
+//! third Elf (with `11000` Calories), then the fifth Elf (with `10000` Calories). The sum of the
+//! Calories carried by these three elves is **`45000`**.
+//!
+//! Find the top three Elves carrying the most Calories. **How many Calories are those Elves
+//! carrying in total?**
 
 use anyhow::Result;
+use itertools::Itertools;
 
 pub const INPUT: &str = include_str!("d01.txt");
 
-pub fn solve_part_one(input: &str) -> Result<i64> {
-    Ok(0)
+pub fn solve_part_one(input: &str) -> Result<u32> {
+    Ok(parse_input(input)?.into_iter().map(|elf| elf.into_iter().sum()).max().unwrap_or(0))
 }
 
-pub fn solve_part_two(input: &str) -> Result<i64> {
-    Ok(0)
+pub fn solve_part_two(input: &str) -> Result<u32> {
+    Ok(parse_input(input)?
+        .into_iter()
+        .map(|elf| elf.into_iter().sum::<u32>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum())
+}
+
+pub fn parse_input(input: &str) -> Result<Vec<Vec<u32>>> {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split_terminator('\n').map(|value| value.parse().map_err(Into::into)).collect()
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
+    use indoc::indoc;
+
     use super::*;
 
-    #[test]
-    fn part_one() {}
+    const INPUT: &str = indoc! {"
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    "};
 
     #[test]
-    fn part_two() {}
+    fn part_one() {
+        assert_eq!(24000, solve_part_one(INPUT).unwrap())
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(45000, solve_part_two(INPUT).unwrap())
+    }
 }
--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -1,4 +1,68 @@
 //! # Year 2022
+//!
+//! <style>
+//! .calendar {
+//!   cursor: default;
+//!   -webkit-user-select: none;
+//!   -khtml-user-select: none;
+//!   -moz-user-select: -moz-none;
+//!   -o-user-select: none;
+//!   user-select: none;
+//!   color: #666666;
+//!   background-color: #0f0f23;
+//! }
+//! .calendar > span {
+//!   color: #333333;
+//! }
+//! .calendar > a {
+//!   text-decoration: none !important;
+//!   color: #666666 !important;
+//!   outline: none;
+//!   cursor: default;
+//! }
+//! .calendar a:hover, .calendar a:focus {
+//!   background-color: #1e1e46;
+//!   background-color: rgba(119,119,165,.2);
+//!   cursor: pointer;
+//! }
+//! .calendar .calendar-day { color: #666666; }
+//! .calendar a .calendar-day { color: #cccccc; }
+//! .calendar a .calendar-mark-complete,
+//! .calendar a .calendar-mark-verycomplete { visibility: hidden; }
+//! .calendar a.calendar-complete     .calendar-mark-complete,
+//! .calendar a.calendar-verycomplete .calendar-mark-complete { visibility: visible; color: #ffff66; }
+//! .calendar a.calendar-verycomplete .calendar-mark-verycomplete { visibility: visible; color: #ffff66; }
+//!
+//! .calendar .calendar-color-u { color:#5eabb4; }
+//! </style>
+//!
+//! <pre class="calendar">
+//! <a href="d25/index.html" class="calendar-day25">#@@@@@@#@@@@##@#@@@#@@#@#@###@@##@#@@@#@@#@@@@@@@  <span class="calendar-day">25</span></a>
+//! <a href="d24/index.html" class="calendar-day24">@#@@@@@@@@@#@##@@@@@#@@@@@@@@@@#@@##@@@@@@@#@@#@#  <span class="calendar-day">24</span></a>
+//! <a href="d23/index.html" class="calendar-day23">@@@@@##@@@#@@##@@@@@##@@#@@#@######@@@#@@@#####@#  <span class="calendar-day">23</span></a>
+//! <a href="d22/index.html" class="calendar-day22">@@#@@@@@@@#@@@@@@#@@##@@#@@#@@@#@##@@@@#@@@#@@@@#  <span class="calendar-day">22</span></a>
+//! <a href="d21/index.html" class="calendar-day21">@@@#@@@#@@@###@@@@@##@@@#@@#@#@@@@@#@##@##@###@@@  <span class="calendar-day">21</span></a>
+//! <a href="d20/index.html" class="calendar-day20">@@@@@@@##@@@####@#@@#@##@##@@@@@@@#@#@#@@@@@@#@@@  <span class="calendar-day">20</span></a>
+//! <a href="d19/index.html" class="calendar-day19">@@#@@@@@@@#@@@#@@@##@@@#@#|#@@#@@##@@@@@@#@@@@###  <span class="calendar-day">19</span></a>
+//! <a href="d18/index.html" class="calendar-day18">@@@@@@@#@@@@@@@@@@@@##@@#@##@@@#@@@@@@@@@#@#@@@@@  <span class="calendar-day">18</span></a>
+//! <a href="d17/index.html" class="calendar-day17">@@@@@@@@@@@@@@@##@@##@@#@@#@@#@@@@@@##@@#@@@@###@  <span class="calendar-day">17</span></a>
+//! <a href="d16/index.html" class="calendar-day16">@@@@@@#@#@#@@#@#@####@@@@@@@@#@@@@#@#@@#@@@##@@@@  <span class="calendar-day">16</span></a>
+//! <a href="d15/index.html" class="calendar-day15">@@###@#@#@@@@@##@@@#@@@#@#@@@@#@#@#@@#@@@@@@##@@@  <span class="calendar-day">15</span></a>
+//! <a href="d14/index.html" class="calendar-day14">#####@@#@@@#@#@@@@@#@@##@#@@@#@@#@@@@@#@##@#@@#@#  <span class="calendar-day">14</span></a>
+//! <a href="d13/index.html" class="calendar-day13">@@@#@#@@@@@@##@@@@@###@##@##@@@@@@@@#@@@@#@@@#@#@  <span class="calendar-day">13</span></a>
+//! <a href="d12/index.html" class="calendar-day12">@@@@#@@@@@##@@@#@@@@@@@@@##@@#@@@@##@@##@@@#@@@#@  <span class="calendar-day">12</span></a>
+//! <a href="d11/index.html" class="calendar-day11">@@#@##@@@@@@@@@#@@@@@@@@@#@##@##@#@@@#@@@@@@@@@@@  <span class="calendar-day">11</span></a>
+//! <a href="d10/index.html" class="calendar-day10">@@@@@@@@#@#@#@@#@@@@@@###@@@@@@@#@@@#@@@@@@@##@#@  <span class="calendar-day">10</span></a>
+//! <a href="d09/index.html" class="calendar-day9">@@@#@@@|@@#@@@@@@#@@@#@@@@@###@@@@@@@#@@#@#@#@@#@  <span class="calendar-day"> 9</span></a>
+//! <a href="d08/index.html" class="calendar-day8">@@@@#@@@@###@@@@@@@@@###@@@#@@@@@@@@@##@@#@@@@#@#  <span class="calendar-day"> 8</span></a>
+//! <a href="d07/index.html" class="calendar-day7">##@@#@####@@@#@@###@#@@@@@#@#@###@@@@@###@#@#@@##  <span class="calendar-day"> 7</span></a>
+//! <a href="d06/index.html" class="calendar-day6">####@@@##@@#@#@@@@@@#@@@#@#@@@@@@#@@@#@@#@@#@@#@@  <span class="calendar-day"> 6</span></a>
+//! <a href="d05/index.html" class="calendar-day5">@@@#@#@##@@#@@@@@#@@@@@@@@@#####@@@@#@#@@@@@@@@@@  <span class="calendar-day"> 5</span></a>
+//! <a href="d04/index.html" class="calendar-day4">@@@#@@@@@@@@#|@##@@#@@#@@##@@@@@###@@@@@@@#@@@#@@  <span class="calendar-day"> 4</span></a>
+//! <a href="d03/index.html" class="calendar-day3">@@@@#@@@@@@@@#@@@#@@##@####@#@@#@@@#@@@#@@#@#@@#@  <span class="calendar-day"> 3</span></a>
+//! <a href="d02/index.html" class="calendar-day2">@@#@@@@@@##@##@##@@@@@@@#@#@@@@#@#@@@#@@@@@@@#@##  <span class="calendar-day"> 2</span><a>
+//! <a href="d01/index.html" class="calendar-day1 calendar-verycomplete"><span class="calendar-color-u">  ~    ~  ~      ~     ~ ~   ~     ~  ~  ~   ~   </span>  <span class="calendar-day"> 1</span> <span class="calendar-mark-complete">*</span><span class="calendar-mark-verycomplete">*</span></a>
+//! </pre>
 
 #![allow(unused_variables)]
 
